fe6f29761d02df6bce3b39c80e5a2a7b6fe399a8
Musoxon <musoxoshimov1997@gmail.com>
2021-10-19 13:46:07 +0500

    task4

---
+++ src/main/java/uz/pdp/task_2_4/Task24Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.task_2_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task24Application.class, args);
+    }
+
+}
